bc4016dad4d8b4d602c5fcb8f1f720cbfcd4d6da
Hans Dulimarta <hans.dulimarta@gmail.com>
2022-03-09 13:12:36 -0500

    vue2-basics: import components directly from main.ts

--- vue2-basics/src/main.ts
+++ vue2-basics/src/main.ts
@@ -1,15 +1,16 @@
 import Vue from "vue";
-import App from "./App.vue";
+// import App from "./App.vue";
 // import App from "./components/00-hello.vue";
-// import App from "./10-data-binding.vue";
-// import App from "./20-forloop.vue";
-// import App from "./22-forloop-objects.vue";
-// import App from "./30-conditional.vue";
-// import App from "./40-input-binding-simple.vue";
-// import App from "./42-input-binding.vue";
-// import App from "./50-event-handling.vue";
-// import App from "./55-event-mods.vue";
-// import App from "./60-props.vue";
+// import App from "./components/10-data-binding.vue";
+// import App from "./components/20-forloop.vue";
+// import App from "./components/22-forloop-objects.vue";
+// import App from "./components/30-conditional.vue";
+// import App from "./components/40-input-binding-simple.vue";
+// import App from "./components/42-input-binding.vue";
+// import App from "./components/50-event-handling.vue";
+// import App from "./components/55-event-mods.vue";
+// import App from "./components/60-props.vue";
+import App from "./components/70-world-time.vue";
 
 Vue.config.productionTip = false;
 
