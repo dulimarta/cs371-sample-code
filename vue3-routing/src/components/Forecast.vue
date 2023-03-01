f15494fe2b80fbacfef2caa233ea453fc45f6e2b
Hans Dulimarta <HansDulimarta@gvsu.edu>
2023-02-28 23:52:18 -0500

    Using component props in Navigation

--- vue3-routing/src/components/Forecast.vue
+++ vue3-routing/src/components/Forecast.vue
@@ -2,9 +2,13 @@
     <div id="top">
     <h2>Forecast</h2>
     <router-link class="link" to="/hourly">Hourly Forecast</router-link>
-
+    <router-link class="link" to="/zip/49401">Allendale</router-link>
+    <router-link class="link" to="/zip/60601">Chicago</router-link>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti eos sapiente odio labore qui adipisci est maiores, nesciunt ut laboriosam atque laborum mollitia beatae nihil totam cumque excepturi eveniet. Hic?</p>
+
+    Your own zip: <input type="text" v-model="inputZip"><button @click="checkAtZip">Check</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsam expedita ut doloribus, dicta temporibus praesentium rerum labore tenetur facere, atque, accusamus fugit delectus dolore obcaecati consectetur iste modi ad.</p>
 
     <button @click="mayIdoHourly">Hourly Prime</button>
 
@@ -12,6 +16,8 @@
 </template>
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
+import {ref} from "vue"
+const inputZip = ref("12345")
 const rt = useRouter()
 function mayIdoHourly() {
     if (Math.random() < 0.5) {
@@ -20,6 +26,10 @@ function mayIdoHourly() {
         alert("You are not a prime member")
     }
 }
+
+function checkAtZip() {
+    rt.push({name: "byZip", params: { zipCode: inputZip.value}})
+}
 </script>
 <style scoped>
 #top {
@@ -27,5 +37,8 @@ function mayIdoHourly() {
     background-color: greenyellow;
     padding: 1em;
     border-radius: 1em;
+}
+.link {
+    margin: 0 2em;
 }
 </style>
--- vue3-routing/src/main.ts
+++ vue3-routing/src/main.ts
@@ -7,12 +7,14 @@ import Home from "./components/Home.vue";
 import Forecast from "./components/Forecast.vue";
 import Settings from "./components/Settings.vue";
 import Hourly from "./components/Hourly.vue";
+import ForecastByZip from "./components/ForecastByZip.vue"
 
 const routes = [
   { path: "/", component: Home },
   { path: "/forecast", component: Forecast },
   { path: "/settings", component: Settings },
   { path: "/hourly", component: Hourly },
+  { name: "byZip", path: "/zip/:zipCode", component: ForecastByZip, props: true}
 ];
 
 const router = createRouter({ routes, history: createWebHashHistory() });
